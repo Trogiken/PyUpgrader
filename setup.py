675f3b2a95f86655719e08dd9d6f30168c3d4ccb
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-06 01:23:18 -0500

    init struct

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='pyupdate',
+    version='0.1.0',
+    author='Noah Blaszak',
+    author_email='70231827+Trogiken@users.noreply.github.com',
+    description='Python library that allows the version of a program to be updated.',
+    packages=['pyupdate'],
+    install_requires=[],
+)
