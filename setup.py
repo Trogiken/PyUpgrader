e7c17ca3a861af96c3c668ee0ecfd6cdbfa4f579
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-07 20:59:14 -0500

    update packages

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Noah Blaszak',
     author_email='70231827+Trogiken@users.noreply.github.com',
     description='Python library that allows the version of a program to be updated.',
-    packages=['pyupdate'],
+    packages=['pyupdate', 'pyupdate.hashing'],
     install_requires=requirements,
     entry_points={
         'console_scripts': [
