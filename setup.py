7a099b58ca8652d732c66dd0b588436f32e58bdf
Noah <70231827+Trogiken@users.noreply.github.com>
2024-01-10 16:34:47 -0500

    Version Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyupdate',
-    version='0.1.0',
+    version='0.2.0',
     author='Noah Blaszak',
     author_email='70231827+Trogiken@users.noreply.github.com',
     description='Python library that allows the version of a program to be updated.',
