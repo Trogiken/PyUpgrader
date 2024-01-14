9f581b9031f2463989d905d8fa4c3c67983f8634
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-01-14 13:17:33 -0500

    Testing Update

--- Testing/main.py
+++ Testing/main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import pyupdate
+
+
+update_man = pyupdate.UpdateManager(r"https://raw.githubusercontent.com/Trogiken/PyUpdate/downloading-cloud-files/Testing/.pyupdate", r"C:\Users\Owner\VScode\PyUpdate\Testing")
+
+
+if __name__ == '__main__':
+    update_check = update_man.check_update()
+    if update_check.get('has_update'):
+        print("Update found!")
+        print(f"{update_check.get('cloud_version')} <- {update_check.get('local_version')}")
+        print(update_check.get('description'))
