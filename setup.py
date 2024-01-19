0434713509128e3acb13d9d5f78ac8a9f3f429bf
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-19 07:50:01 -0500

    Version changed to beta

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyupdate',
-    version='0.3.7',
+    version='1.0.0b1',
     author='Noah Blaszak',
     author_email='70231827+Trogiken@users.noreply.github.com',
     description='Python library that allows the version of a program to be updated.',
