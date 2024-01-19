a407d3be0aed4bbb592364d7c60cdb914c6959a7
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-19 09:57:19 -0500

    classifier fix

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Development Status :: 4 - Beta'
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GPL-3.0 License',
         'Operating System :: OS Independent'
