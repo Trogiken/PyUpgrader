bc23d7040588133db5d075bf13a4ea521da0e62e
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-19 10:38:11 -0500

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='pyupgrader',
-    version='1.0.1b1',
+    version='1.0.2b1',
     author='Noah Blaszak',
     author_email='technology.misc@gmail.com',
     description='Keep your Python code up to date on client machines.',
