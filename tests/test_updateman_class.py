149cf878cbb02832812a10375bed6e7f7173b329
Trogiken <70231827+Trogiken@users.noreply.github.com>
2024-01-30 13:38:38 -0500

    WIP db_sum check working

--- tests/test_updateman_class.py
+++ tests/test_updateman_class.py
@@ -1,16 +1,19 @@
 import os
 import unittest
+import logging
 from unittest.mock import Mock
 from pyupgrader.update import UpdateManager
-from pyupgrader.utilities.helper import Config
+from pyupgrader.utilities.hashing import DBSummary
+from pyupgrader.utilities.helper import Config, normalize_paths
+
+LOGGER = logging.getLogger(__name__)
 
 
 class TestUpdateManager(unittest.TestCase):
     def setUp(self):
-        url = r"https://raw.githubusercontent.com/Trogiken/PyUpgrader/tests-for-update-module/tests/TestingDirectory/.pyupgrader"  # Replace with your info inside the {}
-
-        project_path = os.path.join(os.path.dirname(__file__), "TestingDirectory")
-        self.update_manager = UpdateManager(url, project_path)
+        self.url = r"https://raw.githubusercontent.com/Trogiken/PyUpgrader/tests-for-update-module/tests/TestingDirectory/.pyupgrader"
+        self.project_path = normalize_paths(os.path.join(os.path.dirname(__file__), "TestingDirectory"))
+        self.update_manager = UpdateManager(self.url, self.project_path)
         self.config = Config()
     
     def tearDown(self):
@@ -49,12 +52,19 @@ class TestUpdateManager(unittest.TestCase):
             'local_version': '0.9.0'
         }
 
-        # Assert that the get_config and load_yaml methods were called
-        web_mock.get_config.assert_called_once()
-        config_mock.load_yaml.assert_called_once_with("/path/to/project/.pyupgrader/config.yaml")
-
     def test_db_sum(self):
         """Test the db_sum method of the UpdateManager class."""
+        # Call the db_sum method
+        result = self.update_manager.db_sum()
+
+        LOGGER.debug(f"result: {result}")
+
+        # check if result is a DBSummary object
+        assert isinstance(result, DBSummary)
+
+    def test_get_files(self):
+        # FIXME: This test is failing
+        """Test the get_files method of the UpdateManager class."""
         # Mock the web manager and hashing module
         web_mock = Mock()
         hashing_mock = Mock()
@@ -65,47 +75,22 @@ class TestUpdateManager(unittest.TestCase):
         cloud_hash_db_path = "/path/to/cloud_hashes.db"
         web_mock.download_hash_db.return_value = cloud_hash_db_path
 
-        # Mock the compare_databases method to return a sample DBSummary object
-        db_summary_mock = Mock()
-        hashing_mock.compare_databases.return_value = db_summary_mock
+        # Mock the get_files method to return a sample list of file paths
+        files = ["/path/to/file1.py", "/path/to/file2.py"]
+        hashing_mock.HashDB.return_value.get_file_paths.return_value = files
 
-        # Call the db_sum method
-        result = self.update_manager.db_sum()
+        # Call the get_files method
+        result = self.update_manager.get_files()
 
         # Assert the expected result
-        assert result == db_summary_mock
+        assert result == files
 
-        # Assert that the download_hash_db and compare_databases methods were called
+        # Assert that the download_hash_db and get_file_paths methods were called
         web_mock.download_hash_db.assert_called_once_with("/path/to/cloud_hashes.db")
-        hashing_mock.compare_databases.assert_called_once_with("/path/to/project/.pyupgrader/hash.db", cloud_hash_db_path)
-
-        def test_get_files(self):
-            """Test the get_files method of the UpdateManager class."""
-            # Mock the web manager and hashing module
-            web_mock = Mock()
-            hashing_mock = Mock()
-            self.update_manager._web_man = web_mock
-            self.update_manager._hashing = hashing_mock
-
-            # Mock the download_hash_db method to return a sample cloud hash db path
-            cloud_hash_db_path = "/path/to/cloud_hashes.db"
-            web_mock.download_hash_db.return_value = cloud_hash_db_path
-
-            # Mock the get_files method to return a sample list of file paths
-            files = ["/path/to/file1.py", "/path/to/file2.py"]
-            hashing_mock.HashDB.return_value.get_file_paths.return_value = files
-
-            # Call the get_files method
-            result = self.update_manager.get_files()
-
-            # Assert the expected result
-            assert result == files
-
-            # Assert that the download_hash_db and get_file_paths methods were called
-            web_mock.download_hash_db.assert_called_once_with("/path/to/cloud_hashes.db")
-            hashing_mock.HashDB.return_value.get_file_paths.assert_called_once()
+        hashing_mock.HashDB.return_value.get_file_paths.assert_called_once()
 
     def test_download_files(self):
+        # FIXME: This test is failing
         """Test the download_files method of the UpdateManager class."""
         # Mock the get_files method to return a sample list of file paths
         files = ["/path/to/file1.py", "/path/to/file2.py"]
@@ -125,9 +110,8 @@ class TestUpdateManager(unittest.TestCase):
         assert self.update_manager._web_man.download.call_count == 2
 
     def test_prepare_update(self):
-        """
-        Test the prepare_update method of the UpdateManager class.
-        """
+        """Test the prepare_update method of the UpdateManager class."""
+        # FIXME: This test is failing
         # Mock the get_files method to return a sample list of file paths
         files = ["/path/to/file1.py", "/path/to/file2.py"]
         self.update_manager.get_files = Mock(return_value=files)
