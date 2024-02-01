73c48670202668b7198db17c34c57cee89c503d5
Noah Blaszak <70231827+Trogiken@users.noreply.github.com>
2024-01-31 19:59:23 -0500

    Last tests fixes

--- tests/test_updateman_class.py
+++ tests/test_updateman_class.py
@@ -1,10 +1,14 @@
 import os
 import unittest
 import logging
+import pytest
+import pickle
+import shutil
 from unittest.mock import Mock
 from pyupgrader.update import UpdateManager
 from pyupgrader.utilities.hashing import DBSummary
 from pyupgrader.utilities.helper import Config, normalize_paths
+from pyupgrader.update import NoUpdateError
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,13 +19,9 @@ class TestUpdateManager(unittest.TestCase):
         self.project_path = normalize_paths(os.path.join(os.path.dirname(__file__), "TestingDirectory"))
         self.update_manager = UpdateManager(self.url, self.project_path)
         self.config = Config()
-    
-    def tearDown(self):
-        pass
 
     def test_check_update(self):
         """Test the check_update method of the UpdateManager class."""
-        # Mock the web manager and config manager
         web_mock = Mock()
         config_mock = Mock()
         self.update_manager._web_man = web_mock
@@ -41,10 +41,8 @@ class TestUpdateManager(unittest.TestCase):
         }
         config_mock.load_yaml.return_value = local_config
 
-        # Call the check_update method
         result = self.update_manager.check_update()
 
-        # Assert the expected result
         assert result == {
             'has_update': True,
             'description': 'Sample update description',
@@ -54,77 +52,59 @@ class TestUpdateManager(unittest.TestCase):
 
     def test_db_sum(self):
         """Test the db_sum method of the UpdateManager class."""
-        # Call the db_sum method
         result = self.update_manager.db_sum()
-
         LOGGER.debug(f"result: {result}")
 
         # check if result is a DBSummary object
         assert isinstance(result, DBSummary)
 
     def test_get_files(self):
-        # FIXME: This test is failing
         """Test the get_files method of the UpdateManager class."""
-        # Mock the web manager and hashing module
-        web_mock = Mock()
         hashing_mock = Mock()
-        self.update_manager._web_man = web_mock
-        self.update_manager._hashing = hashing_mock
-
-        # Mock the download_hash_db method to return a sample cloud hash db path
-        cloud_hash_db_path = "/path/to/cloud_hashes.db"
-        web_mock.download_hash_db.return_value = cloud_hash_db_path
 
         # Mock the get_files method to return a sample list of file paths
-        files = ["/path/to/file1.py", "/path/to/file2.py"]
+        files = ['ImADirectory/loader.py', 'ImADirectory/moreData.pkl', 'data.yml', 'start_me.py']
         hashing_mock.HashDB.return_value.get_file_paths.return_value = files
 
-        # Call the get_files method
         result = self.update_manager.get_files()
-
-        # Assert the expected result
         assert result == files
 
-        # Assert that the download_hash_db and get_file_paths methods were called
-        web_mock.download_hash_db.assert_called_once_with("/path/to/cloud_hashes.db")
-        hashing_mock.HashDB.return_value.get_file_paths.assert_called_once()
-
     def test_download_files(self):
-        # FIXME: This test is failing
         """Test the download_files method of the UpdateManager class."""
-        # Mock the get_files method to return a sample list of file paths
-        files = ["/path/to/file1.py", "/path/to/file2.py"]
-        self.update_manager.get_files = Mock(return_value=files)
+        temp_dir = os.path.join(os.path.dirname(__file__), "temp")
+        self.update_manager.download_files(temp_dir)
 
-        # Mock the download method to do nothing
-        self.update_manager._web_man.download = Mock()
+        # check that the correct files were downloaded
+        assert os.path.exists(os.path.join(temp_dir, "ImADirectory", "loader.py"))
+        assert os.path.exists(os.path.join(temp_dir, "ImADirectory", "moreData.pkl"))
+        assert os.path.exists(os.path.join(temp_dir, "data.yml"))
+        assert os.path.exists(os.path.join(temp_dir, "start_me.py"))
 
-        # Call the download_files method
-        result = self.update_manager.download_files()
+        shutil.rmtree(temp_dir)
 
-        # Assert the expected result
-        assert result == "/path/to/project/.pyupgrader"
-
-        # Assert that the get_files and download methods were called
-        self.update_manager.get_files.assert_called_once()
-        assert self.update_manager._web_man.download.call_count == 2
 
     def test_prepare_update(self):
         """Test the prepare_update method of the UpdateManager class."""
-        # FIXME: This test is failing
-        # Mock the get_files method to return a sample list of file paths
-        files = ["/path/to/file1.py", "/path/to/file2.py"]
-        self.update_manager.get_files = Mock(return_value=files)
-
         # Mock the download_files method to do nothing
         self.update_manager.download_files = Mock()
 
-        # Call the prepare_update method
         result = self.update_manager.prepare_update()
+        with open(result, "rb") as f:
+            data = pickle.load(f)
+            LOGGER.debug(f"data: {data}")
+        
+        actual_data = {
+            'update': data['update'],
+            'project_path': data['project_path'],
+            'startup_path': data['startup_path'],
+            'cleanup': data['cleanup'],
+        }
 
-        # Assert the expected result
-        assert result == "/path/to/project/.pyupgrader"
+        expected_data = {
+            'update': ['ImADirectory/loader.py', 'ImADirectory/moreData.pkl', 'data.yml', 'start_me.py'],
+            'project_path': self.project_path,
+            'startup_path': os.path.join(self.project_path, "start_me.py"),
+            'cleanup': True,
+        }
 
-        # Assert that the get_files and download_files methods were called
-        self.update_manager.get_files.assert_called_once()
-        self.update_manager.download_files.assert_called_once()
+        assert actual_data == expected_data
