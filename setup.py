ec168f2fade8c6dbd74d0c75afb6f9a7361b75f9
Noah <70231827+Trogiken@users.noreply.github.com>
2024-02-01 20:28:04 -0500

    Update setup.py
    #15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding="utf-8") as f:
 
 setup(
     name='pyupgrader',
-    version='2.1.9b1',
+    version='2.1.10b1',
     author='Noah Blaszak',
     author_email='technology.misc@gmail.com',
     description='Keep your Python code up to date on client machines.',
